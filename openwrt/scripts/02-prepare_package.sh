c676ee2936c5f748bc11d3d174bc3dac5f1baafc
ilxp <lixp@live.com>
2025-02-15 13:25:32 +0800

    up

---
+++ openwrt/scripts/02-prepare_package.sh
@@ -0,0 +1,105 @@
+#!/bin/bash -e
+
+# golang 1.23
+rm -rf feeds/packages/lang/golang
+git clone https://$github/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
+# node - prebuilt
+rm -rf feeds/packages/lang/node
+git clone https://$github/sbwml/feeds_packages_lang_node-prebuilt feeds/packages/lang/node -b packages-24.10
+
+# default settings
+git clone https://$github/sbwml/default-settings package/new/default-settings -b openwrt-24.10
+
+# wwan
+git clone https://github.com/sbwml/wwan-packages package/new/wwan
+
+# ddns - fix boot
+sed -i '/boot()/,+2d' feeds/packages/net/ddns-scripts/files/etc/init.d/ddns
+
+# nlbwmon - disable syslog
+sed -i 's/stderr 1/stderr 0/g' feeds/packages/net/nlbwmon/files/nlbwmon.init
+
+# pcre - 8.45
+mkdir -p package/libs/pcre
+curl -s $mirror/openwrt/patch/pcre/Makefile > package/libs/pcre/Makefile
+curl -s $mirror/openwrt/patch/pcre/Config.in > package/libs/pcre/Config.in
+
+# lrzsz - 0.12.20
+rm -rf feeds/packages/utils/lrzsz
+git clone https://$github/sbwml/packages_utils_lrzsz package/new/lrzsz
+
+# irqbalance: disable build with numa
+curl -s $mirror/openwrt/patch/irqbalance/011-meson-numa.patch > feeds/packages/utils/irqbalance/patches/011-meson-numa.patch
+sed -i '/-Dcapng=disabled/i\\t-Dnuma=disabled \\' feeds/packages/utils/irqbalance/Makefile
+
+# samba4 - bump version
+rm -rf feeds/packages/net/samba4
+git clone https://$github/sbwml/feeds_packages_net_samba4 feeds/packages/net/samba4
+# liburing - 2.7 (samba-4.21.0)
+rm -rf feeds/packages/libs/liburing
+git clone https://$github/sbwml/feeds_packages_libs_liburing feeds/packages/libs/liburing
+# enable multi-channel
+sed -i '/workgroup/a \\n\t## enable multi-channel' feeds/packages/net/samba4/files/smb.conf.template
+sed -i '/enable multi-channel/a \\tserver multi channel support = yes' feeds/packages/net/samba4/files/smb.conf.template
+# default config
+sed -i 's/#aio read size = 0/aio read size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#aio write size = 0/aio write size = 0/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/bind interfaces only = yes/bind interfaces only = no/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#create mask/create mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/#directory mask/directory mask/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/0666/0644/g;s/0744/0755/g;s/0777/0755/g' feeds/luci/applications/luci-app-samba4/htdocs/luci-static/resources/view/samba4.js
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/samba.config
+sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/smb.conf.template
+# rk3568 bind cpus
+[ "$platform" = "rk3568" ] && sed -i 's#/usr/sbin/smbd -F#/usr/bin/taskset -c 1,0 /usr/sbin/smbd -F#' feeds/packages/net/samba4/files/samba.init
+
+
+
+# nethogs
+git clone https://github.com/sbwml/package_new_nethogs package/new/nethogs
+
+# Theme
+git clone --depth 1 https://$github/jerrykuku/luci-theme-argon package/new/luci-theme-argon
+git clone --depth 1 https://$github/jerrykuku/luci-app-argon-config package/new/luci-app-argon-config
+
+# Mosdns
+git clone https://$github/sbwml/luci-app-mosdns -b v5 package/new/mosdns
+
+# iperf3
+sed -i "s/D_GNU_SOURCE/D_GNU_SOURCE -funroll-loops/g" feeds/packages/net/iperf3/Makefile
+
+# nlbwmon
+sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
+sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
+
+# custom packages
+rm -rf feeds/packages/utils/coremark
+git clone https://$github/sbwml/openwrt_pkgs package/new/custom --depth=1
+# coremark - prebuilt with gcc15
+if [ "$platform" = "rk3568" ]; then
+    curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-4-threads > package/new/custom/coremark/src/musl/coremark.aarch64
+elif [ "$platform" = "rk3399" ]; then
+    curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-6-threads > package/new/custom/coremark/src/musl/coremark.aarch64
+elif [ "$platform" = "armv8" ]; then
+    curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-16-threads > package/new/custom/coremark/src/musl/coremark.aarch64
+fi
+
+# luci-compat - fix translation
+sed -i 's/<%:Up%>/<%:Move up%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
+sed -i 's/<%:Down%>/<%:Move down%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
+
+# SQM Translation
+mkdir -p feeds/packages/net/sqm-scripts/patches
+curl -s $mirror/openwrt/patch/sqm/001-help-translation.patch > feeds/packages/net/sqm-scripts/patches/001-help-translation.patch
+
+# unzip
+rm -rf feeds/packages/utils/unzip
+git clone https://$github/sbwml/feeds_packages_utils_unzip feeds/packages/utils/unzip
+
+# tcp-brutal
+git clone https://$github/sbwml/package_kernel_tcp-brutal package/kernel/tcp-brutal
+
+# watchcat - clean config
+true > feeds/packages/utils/watchcat/files/watchcat.config
